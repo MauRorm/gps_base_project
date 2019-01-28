69f303bc6683e65b0a6b6758cb6a9d80cf7239ad
hades <mauegg@gmail.com>
2019-01-27 23:33:42 -0600

    first commit :)"

---
+++ app/containers/HomePage/HomePage.js
@@ -0,0 +1,76 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import './style.scss';
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  /**
+   * when initial state username is not null, submit the form to load repos
+   */
+
+   constructor(props) {
+    super(props);
+    this.state = {
+      input:'',
+    };
+   }
+
+  componentDidMount() {
+    if (this.props.username && this.props.username.trim().length > 0) {
+      this.props.onSubmitForm();
+    }
+  }
+
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch")
+  }
+
+  render() {
+    const { loading, error, repos } = this.props;
+    const reposListProps = {
+      loading,
+      error,
+      repos,
+    };
+    return (
+      <article>
+        <Helmet>
+          <title>Home Page</title>
+          <meta name="description" content="A React.js Boilerplate application homepage" />
+        </Helmet>
+        <div className="home-page">
+          <p>Valor</p>
+          <input
+            value={this.state.input}
+            onChange={(event)=>{
+              this.setState({
+                input:event.target.value
+              })
+            }}
+          />
+        </div>
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+  repos: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.bool,
+  ]),
+  onSubmitForm: PropTypes.func,
+  username: PropTypes.string,
+  onChangeUsername: PropTypes.func,
+};
